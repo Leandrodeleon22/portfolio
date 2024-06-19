ccde6fb7da53616df016f921f5e18179d970e54c
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-06-18 22:11:21 -0400

    edit title header size to become responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,10 +12,14 @@ const Header = () => {
       <NavBar />
       <Wrapper>
         <div className=" text-[9.9rem] font-extralight" id="home">
-          <div className="min-h-screen flex items-center">
-            <div className="mt-[-11rem] text-[9.9rem] font-extralight">
-              <p className=" m-0">Web Developer</p>
-              <p className="text-[7.7rem] leading-[7.5rem]">& Designer</p>
+          <div className="min-h-screen flex items-center max-[630px]:justify-center ">
+            <div className="mt-[-11rem] text-[9.9rem] font-extralight max-[630px]:text-center">
+              <p className=" m-0 max-[832px]:text-[7.5rem] max-[630px]:text-[6rem]">
+                Web Developer
+              </p>
+              <p className="text-[7.7rem] leading-[7.5rem max-[832px]:text-[6.5rem] max-[630px]:text-[4rem] ">
+                & Designer
+              </p>
             </div>
           </div>
         </div>
