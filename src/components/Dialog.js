a3adee5e9239be7b5c2ff479fb322c34bfe75cfc
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-06-20 21:33:47 -0400

    add suspense in dialog component

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useRef } from "react";
+import React, { Suspense, useEffect, useRef } from "react";
 import checkIcon from "@/assets/DESKTOP/check.png";
 
 export default function Dialog() {
@@ -28,39 +28,41 @@ export default function Dialog() {
 
   const dialog =
     showDialog === "y" ? (
-      <dialog
-        ref={dialogRef}
-        className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10  rounded-xl backdrop:bg-gray-800/50 overflow-visible"
-      >
-        <div className=" min-w-full bg-gray-200 flex flex-col overflow-visible px-20">
-          <div className=" bg-slate-200  overflow-visible rounded-xl ">
-            <div className="flex justify-center">
-              <Image
-                src={checkIcon}
-                width={100}
-                height={100}
-                alt="check icon"
-                className="mt-[-3.4em] overflow-visible"
-              />
-            </div>
+      <Suspense>
+        <dialog
+          ref={dialogRef}
+          className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10  rounded-xl backdrop:bg-gray-800/50 overflow-visible"
+        >
+          <div className=" min-w-full bg-gray-200 flex flex-col overflow-visible px-20">
+            <div className=" bg-slate-200  overflow-visible rounded-xl ">
+              <div className="flex justify-center">
+                <Image
+                  src={checkIcon}
+                  width={100}
+                  height={100}
+                  alt="check icon"
+                  className="mt-[-3.4em] overflow-visible"
+                />
+              </div>
 
-            <div className="my-8">
-              <h1 className="text-[4rem] text-center font-medium my-6">
-                Thank You!
-              </h1>
-              <p className="text-center text-[2.25rem] mb-8 ">
-                Your message has been sent successfully!
-              </p>
-              <button
-                onClick={closeDialog}
-                className="w-full bg-[var(--neon-green-color)] my-10 h-[4rem] rounded-lg text-[2rem] font-medium text-white hover:bg-green-500"
-              >
-                OK
-              </button>
+              <div className="my-8">
+                <h1 className="text-[4rem] text-center font-medium my-6">
+                  Thank You!
+                </h1>
+                <p className="text-center text-[2.25rem] mb-8 ">
+                  Your message has been sent successfully!
+                </p>
+                <button
+                  onClick={closeDialog}
+                  className="w-full bg-[var(--neon-green-color)] my-10 h-[4rem] rounded-lg text-[2rem] font-medium text-white hover:bg-green-500"
+                >
+                  OK
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </dialog>
+        </dialog>
+      </Suspense>
     ) : null;
 
   return dialog;
