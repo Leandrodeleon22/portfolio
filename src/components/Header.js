f11bebbbc8266e5e198784efc403bcc8030c7fd1
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-07-04 08:43:42 -0400

    add typewriter effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
-import Image from "next/image";
+"use client";
 import NavBar from "./NavBar";
-
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import Wrapper from "./Wrapper";
 import MobileNavBar from "./MobileNavBar";
 
 const Header = () => {
+  const strText = "Web Developer\n& Designer";
+  const [text] = useTypewriter({
+    words: [strText],
+    loop: 2,
+  });
+
   return (
     <>
       {<MobileNavBar />}
@@ -14,12 +20,11 @@ const Header = () => {
         <div className=" text-[9.9rem] font-extralight" id="home">
           <div className="min-h-screen flex items-center max-[630px]:justify-center ">
             <div className=" text-[9.9rem] font-extralight max-[630px]:text-center">
-              <p className=" m-0 max-[832px]:text-[7.5rem] max-[630px]:text-[6rem] max-[500px]:text-[4rem] ">
-                Web Developer
+              <p className=" m-0 max-[832px]:text-[7rem] max-[630px]:text-[5.5rem] max-[500px]:text-[3.5rem] ">
+                {text}
+                <Cursor />
               </p>
-              <p className="text-[7.7rem] leading-[7.5rem max-[832px]:text-[6.5rem] max-[630px]:text-[4rem] ">
-                & Designer
-              </p>
+              <p className="text-[7.7rem] leading-[7.5rem max-[832px]:text-[6.5rem] max-[630px]:text-[4rem] "></p>
             </div>
           </div>
         </div>
