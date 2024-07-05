ead13105d92c6a8cb15d96ad2f918f79c778b997
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-07-05 09:38:39 -0400

    add animation in modal

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useRef } from "react";
 import checkIcon from "@/assets/DESKTOP/check.png";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Dialog() {
   const searchParams = useSearchParams();
@@ -26,42 +27,86 @@ export default function Dialog() {
     // onClose();
   };
 
-  const dialog =
-    showDialog === "y" ? (
-      <dialog
-        ref={dialogRef}
-        className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10  rounded-xl backdrop:bg-gray-800/50 overflow-visible"
-      >
-        <div className=" min-w-full bg-gray-200 flex flex-col overflow-visible px-20">
-          <div className=" bg-slate-200  overflow-visible rounded-xl ">
-            <div className="flex justify-center">
-              <Image
-                src={checkIcon}
-                width={100}
-                height={100}
-                alt="check icon"
-                className="mt-[-3.4em] overflow-visible"
-              />
-            </div>
+  // const dialog =
+  //   showDialog === "y" ? (
+  //     <dialog
+  //       ref={dialogRef}
+  //       className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10  rounded-xl backdrop:bg-gray-800/50 overflow-visible"
+  //     >
+  //       <div className=" min-w-full bg-gray-200 flex flex-col overflow-visible px-20">
+  //         <div className=" bg-slate-200  overflow-visible rounded-xl ">
+  //           <div className="flex justify-center">
+  //             <Image
+  //               src={checkIcon}
+  //               width={100}
+  //               height={100}
+  //               alt="check icon"
+  //               className="mt-[-3.4em] overflow-visible"
+  //             />
+  //           </div>
 
-            <div className="my-8">
-              <h1 className="text-[4rem] text-center font-medium my-6">
-                Thank You!
-              </h1>
-              <p className="text-center text-[2.25rem] mb-8 ">
-                Your message has been sent successfully!
-              </p>
-              <button
-                onClick={closeDialog}
-                className="w-full bg-[var(--neon-green-color)] my-10 h-[4rem] rounded-lg text-[2rem] font-medium text-white hover:bg-green-500"
-              >
-                OK
-              </button>
+  //           <div className="my-8">
+  //             <h1 className="text-[4rem] text-center font-medium my-6">
+  //               Thank You!
+  //             </h1>
+  //             <p className="text-center text-[2.25rem] mb-8 ">
+  //               Your message has been sent successfully!
+  //             </p>
+  //             <button
+  //               onClick={closeDialog}
+  //               className="w-full bg-[var(--neon-green-color)] my-10 h-[4rem] rounded-lg text-[2rem] font-medium text-white hover:bg-green-500"
+  //             >
+  //               OK
+  //             </button>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </dialog>
+  //   ) : null;
+
+  // return dialog;
+
+  return (
+    <AnimatePresence>
+      {showDialog === "y" && (
+        <motion.dialog
+          initial={{ opacity: 0, y: "-10%" }}
+          animate={{ opacity: 1, y: "5%" }}
+          transition={{ delay: 0.2 }}
+          exit={{ opacity: 0 }}
+          ref={dialogRef}
+          className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10  rounded-xl backdrop:bg-gray-800/50 overflow-visible"
+        >
+          <div className=" min-w-full bg-gray-200 flex flex-col overflow-visible px-20">
+            <div className=" bg-slate-200  overflow-visible rounded-xl ">
+              <div className="flex justify-center">
+                <Image
+                  src={checkIcon}
+                  width={100}
+                  height={100}
+                  alt="check icon"
+                  className="mt-[-3.4em] overflow-visible"
+                />
+              </div>
+
+              <div className="my-8">
+                <h1 className="text-[4rem] text-center font-medium my-6">
+                  Thank You!
+                </h1>
+                <p className="text-center text-[2.25rem] mb-8 ">
+                  Your message has been sent successfully!
+                </p>
+                <button
+                  onClick={closeDialog}
+                  className="w-full bg-[var(--neon-green-color)] my-10 h-[4rem] rounded-lg text-[2rem] font-medium text-white hover:bg-green-500"
+                >
+                  OK
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </dialog>
-    ) : null;
-
-  return dialog;
+        </motion.dialog>
+      )}
+    </AnimatePresence>
+  );
 }
